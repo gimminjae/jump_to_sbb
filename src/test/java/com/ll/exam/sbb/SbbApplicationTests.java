8e8d5f7a0172458ced8c887546cbc9dc1826407f
gimminjae <min356812@naver.com>
2022-08-10 16:02:36 +0900

    testJpa7 : 삭제

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -22,7 +22,7 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa() {
+	void testJpa() { //추가
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
 		q1.setContent("sbb에 대해서 알고 싶네요.");
@@ -40,7 +40,7 @@ class SbbApplicationTests {
 	}
 
 	@Test
-	void testJpa2() {
+	void testJpa2() { //조회
 		List<Question> all = questionRepository.findAll(); //select * from question;
 		assertThat(2).isEqualTo(all.size());
 
@@ -48,23 +48,23 @@ class SbbApplicationTests {
 		assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
 	}
 	@Test
-	void testJpa3() {
+	void testJpa3() { //주제로 조회
 		Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertThat(q.getId()).isEqualTo(1);
 	}
 	@Test
-	void testJpa4() {
+	void testJpa4() { //주제와 내용으로 조회
 		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶네요.");
 		assertThat(q.getId()).isEqualTo(1);
 	}
 	@Test
-	void testJpa5() {
+	void testJpa5() { //sbb로 시작하는 주제로 조회
 		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
 		Question q = qList.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 	@Test
-	void testJpa6() {
+	void testJpa6() { //id로 조회 및 수정
 		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
@@ -72,5 +72,14 @@ class SbbApplicationTests {
 		this.questionRepository.save(q);
 	}
 
+	@Test
+	void testJpa7() { //삭제
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
 
 }
